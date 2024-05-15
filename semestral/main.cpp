4bad09143c401e15559616fac31c31f45bd016d3
Stefan Slavkovsky <slavkovsky.stefan01@gmail.com>
2024-05-15 12:10:37 +0200

    Minor improvements

--- semestral/main.cpp
+++ semestral/main.cpp
@@ -4,29 +4,14 @@
 #include "games.h"
 
 
-//int main() {
-//    TicTacToe state{};
-//
-//    std::cout << state;
-//    while(!state.is_final()) {
-//
-//        auto r = minimax(state, state.turn == 0);
-//        std::cout << "Utility: " << r.utility << std::endl << std::endl;
-//
-//        if(!r.best_response.has_value())
-//            break;
-//        state = r.best_response.value();
-//        std::cout << state;
-//    }
-//}
-
-int main() {
-    Checkers state{};
+template<typename game>
+void run_game() {
+    game state{};
 
     std::cout << state;
     while(!state.is_final()) {
 
-        auto r = minimax(state, state.turn == 0, 12);
+        auto r = minimax(state, state.turn == 0, 7);
         std::cout << "Utility: " << r.utility << std::endl << std::endl;
 
         if(!r.best_response.has_value())
@@ -34,4 +19,10 @@ int main() {
         state = r.best_response.value();
         std::cout << state;
     }
+}
+
+
+int main() {
+//    run_game<TicTacToe>();
+    run_game<Checkers>();
 }
